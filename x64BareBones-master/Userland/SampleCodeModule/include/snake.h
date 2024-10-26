4cd3cedd8b387229373f972b86c1ac864cbc183a
maraujofeltrin <manuel.araujofeltrin@gmail.com>
2024-10-26 15:42:39 -0300

    Empezamos con el juego
    Co-Authored-By: santinopepe <146956051+santinopepe@users.noreply.github.com>
    Co-Authored-By: Santiago Nogueira <147627792+snogueira1@users.noreply.github.com>

---
+++ x64BareBones-master/Userland/SampleCodeModule/include/snake.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+void drawTrain(int size, int x, int y);
+
+typedef struct{
+    int x, y;
+    int inc_x, inc_y;
+    int points;
+    //Color color;
+    //char * name;
+} Player;
